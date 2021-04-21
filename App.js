0711cf59486b5af4692ff4805c3cfbdc07c2dde4
udaymondal <udaysk.mondal@gmail.com>
2021-04-21 18:18:36 +0600

    boiler plate

--- App.js
+++ App.js
@@ -2,27 +2,10 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Shadow Ninja');
-  const [age, setAge] = useState('30');
-
+  
   return (
     <View style={styles.container}>
-      <Text>Enter Name:</Text>
-      <TextInput
-        style={styles.input}
-        multiline
-        placeholder='e.g John Doe'
-        onChangeText={(val) => setName(val)}
-      />
-      <Text>Enter Age:</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType='numeric'
-        placeholder='e.g 25'
-        onChangeText={(val) => setAge(val)}
-      />
-      <TextInput>name: {name} age: {age}</TextInput>
-
+      
     </View>
   );
 }
@@ -35,11 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'goldenrod'
   },
-  input: {
-    borderWidth: 1,
-    borderColor: 'white',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
+  
 });
