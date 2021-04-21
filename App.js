c3ec79b7e4016fe68f73ff0be4b444db29a89d91
udaymondal <udaysk.mondal@gmail.com>
2021-04-21 13:54:14 +0600

    state change

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('Shadow Ninja');
+  const [person, setPerson] = useState({name: "Mario", age: 55})
+
+  const clickHandler = () =>{
+    setName('Test No yesterday');
+    setPerson({name: 'Luigi', age: 30});
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name: {name}</Text>
+      <Text>Itz me {person.name} and I am {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -17,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'goldenrod'
   },
+  buttonContainer:{
+    marginTop: 20
+  }
 });
