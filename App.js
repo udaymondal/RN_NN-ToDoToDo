fe1fa306ce3becef844e3512d479e3f1f6f6b649
udaymondal <udaysk.mondal@gmail.com>
2021-04-21 14:37:48 +0600

    text-input

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('Shadow Ninja');
-  const [person, setPerson] = useState({name: "Mario", age: 55})
-
-  const clickHandler = () =>{
-    setName('Test No yesterday');
-    setPerson({name: 'Luigi', age: 30});
-  }
+  const [age, setAge] = useState('30');
 
   return (
     <View style={styles.container}>
-      <Text>My name: {name}</Text>
-      <Text>Itz me {person.name} and I am {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler} />
-      </View>
+      <Text></Text>
+      <Text>name: {name} age: {age}</Text>
+      
     </View>
   );
 }
@@ -29,7 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'goldenrod'
   },
-  buttonContainer:{
-    marginTop: 20
-  }
+
 });
