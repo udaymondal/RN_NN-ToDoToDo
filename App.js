e168c56be45e7afcdf3eaeb5bc083d8791b7bcaf
udaymondal <udaysk.mondal@gmail.com>
2021-04-23 18:43:56 +0600

    col added

--- App.js
+++ App.js
@@ -3,28 +3,30 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'ninja', key: '1' },
-    { name: 'shau', key: '2' },
-    { name: 'blade', key: '3' },
-    { name: 'goku', key: '4' },
-    { name: 'vegeta', key: '5' },
-    { name: 'naruto', key: '6' },
-    { name: 'eren', key: '7' },
-    { name: 'saskeu', key: '8' },
-    { name: 'gohan', key: '9' },
-    { name: 'chichi', key: '10' }
+    { name: 'ninja', id: '1' },
+    { name: 'shau', id: '2' },
+    { name: 'blade', id: '3' },
+    { name: 'goku', id: '4' },
+    { name: 'vegeta', id: '5' },
+    { name: 'naruto', id: '6' },
+    { name: 'eren', id: '7' },
+    { name: 'saskeu', id: '8' },
+    { name: 'gohan', id: '9' },
+    { name: 'chichi', id: '10' }
   ])
 
   return (
     <View style={styles.container}>
 
       <FlatList
+        keyExtractor={item => item.id}
+        numColumns={2}
         data={people}
         renderItem={({ item }) => (
           <Text style={styles.item}>{item.name.toUpperCase()}</Text>
         )}
       />
-      
+
     </View>
   );
 }
@@ -41,8 +43,10 @@ const styles = StyleSheet.create({
   item: {
     marginTop: 24,
     padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24
+    backgroundColor: '#03DAC5',
+    fontSize: 24,
+    marginHorizontal:10,
+    marginTop: 24,
   }
 
 });
